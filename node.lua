0f90d2ca9a0314280f7306a6cf7bd84dbdf69e48
Jan Henrik Sawatzki <jhs@sawatzki-home.de>
2020-06-24 10:42:48 +0200

    New options to hide/show info

--- node.lua
+++ node.lua
@@ -35,6 +35,10 @@ local node_sensors = {}
 local temparature_identifier = "Temperature"
 local humidity_identifier = "Humidity"
 local dew_point_identifier = "Dew point"
+local time_identifier = "Time"
+local type_identifier = "Type"
+local sensor_identifier = "Sensor"
+local name_identifier = "Name"
 
 local temperature_unit = "°C"
 
@@ -49,69 +53,66 @@ local sensors_vertically = 1
 
 --sensors_per_view = w * h
 
-util.json_watch("config.json", function(config)
-    node_config = config
-    font = resource.load_font(node_config.font.asset_name)
-    if next(node_i18n) ~= nil then
+local function i18n(config)
+    if next(config) ~= nil then
         if node_config.language == "en" then
             temparature_identifier = node_i18n.temparature_identifier.en
             humidity_identifier = node_i18n.humidity_identifier.en
             dew_point_identifier = node_i18n.dew_point_identifier.en
+            time_identifier = node_i18n.time_identifier.en
+            type_identifier = node_i18n.type_identifier.en
+            sensor_identifier = node_i18n.sensor_identifier.en
+            name_identifier = node_i18n.name_identifier.en
         elseif node_config.language == "de" then
             temparature_identifier = node_i18n.temparature_identifier.de
             humidity_identifier = node_i18n.humidity_identifier.de
             dew_point_identifier = node_i18n.dew_point_identifier.de
+            time_identifier = node_i18n.time_identifier.de
+            type_identifier = node_i18n.type_identifier.de
+            sensor_identifier = node_i18n.sensor_identifier.de
+            name_identifier = node_i18n.name_identifier.de
         else
             -- Fallback
             temparature_identifier = node_i18n.temparature_identifier.en
             humidity_identifier = node_i18n.humidity_identifier.en
             dew_point_identifier = node_i18n.dew_point_identifier.en
+            time_identifier = node_i18n.time_identifier.en
+            type_identifier = node_i18n.type_identifier.en
+            sensor_identifier = node_i18n.sensor_identifier.en
+            name_identifier = node_i18n.name_identifier.en
         end
     else
         -- Fallback
         temparature_identifier = "Temperature"
         humidity_identifier = "Humidity"
         dew_point_identifier = "Dew point"
+        time_identifier = "Time"
+        type_identifier = "Type"
+        sensor_identifier = "Sensor"
+        name_identifier = "Name"
     end
+end
+
+util.json_watch("config.json", function(config)
+    node_config = config
+    font = resource.load_font(node_config.font.asset_name)
+    if node_config.temperature_unit == "celsius" then
+        temperature_unit = "°C"
+    elseif node_config.temperature_unit == "fahrenheit" then
+        temperature_unit = "°F"
+    else
+        temperature_unit = "K"
+    end
+    i18n(node_i18n)
 end)
 
 util.json_watch("i18n.json", function(i18n)
     node_i18n = i18n
-    if next(node_config) ~= nil then
-        if node_config.language == "en" then
-            temparature_identifier = node_i18n.temparature_identifier.en
-            humidity_identifier = node_i18n.humidity_identifier.en
-            dew_point_identifier = node_i18n.dew_point_identifier.en
-        elseif node_config.language == "de" then
-            temparature_identifier = node_i18n.temparature_identifier.de
-            humidity_identifier = node_i18n.humidity_identifier.de
-            dew_point_identifier = node_i18n.dew_point_identifier.de
-        else
-            -- Fallback
-            temparature_identifier = node_i18n.temparature_identifier.en
-            humidity_identifier = node_i18n.humidity_identifier.en
-            dew_point_identifier = node_i18n.dew_point_identifier.en
-        end
-    else
-        -- Fallback
-        temparature_identifier = "Temperature"
-        humidity_identifier = "Humidity"
-        dew_point_identifier = "Dew point"
-    end
+    i18n(node_config)
 end)
 
 util.json_watch("sensors.json", function(sensors)
     node_sensors = sensors
-
-    if next(node_sensors) ~= nil then
-        if node_sensors[1].sensor_temperature_unit == "celsius" then
-            temperature_unit = "°C"
-        elseif node_sensors[1].sensor_temperature_unit == "fahrenheit" then
-            temperature_unit = "°F"
-        else
-            temperature_unit = "K"
-        end
-    end
 end)
 
 local function Clock()
@@ -150,6 +151,14 @@ end
 
 local clock = Clock()
 
+local function isBitSet(number, bit)
+    if number & (1 << (bit - 1)) then
+        return true
+    else
+        return false
+    end
+end
+
 function node.render()
     gl.clear(node_config.bg_color.r, node_config.bg_color.g, node_config.bg_color.b, node_config.bg_color.a)
 --Different 
@@ -157,13 +166,33 @@ function node.render()
     local system_time_string = "System Time: " ..  clock.formatted()
 --    local time_width = font:width(time_string, 100)
 --    local time_x = (NATIVE_WIDTH/2)-(time_width/2)
-    font:write(20, 10, system_time_string, 40, node_config.font_color.r, node_config.font_color.g, node_config.font_color.b, node_config.font_color.a)
+    local y_gap = 10
+    local font_size = 40
+    local y_pos = y_gap
+    font:write(20, y_pos, system_time_string, font_size, node_config.font_color.r, node_config.font_color.g, node_config.font_color.b, node_config.font_color.a)
+    y_pos += font_size + y_gap
     if next(node_sensors) ~= nil then
-        font:write(20, 210, "Sensor Name: " .. node_sensors[1].sensor_title, 40, node_config.font_color.r, node_config.font_color.g, node_config.font_color.b, node_config.font_color.a)
-        font:write(20, 310, "Sensor Type: " .. node_sensors[1].sensor_type, 40, node_config.font_color.r, node_config.font_color.g, node_config.font_color.b, node_config.font_color.a)
-        font:write(20, 410, "Sensor Time: " .. node_sensors[1].sensor_time, 40, node_config.font_color.r, node_config.font_color.g, node_config.font_color.b, node_config.font_color.a)
-        font:write(20, 510, "Sensor " .. temparature_identifier .. ": " .. node_sensors[1].values.temperature .. " " .. temperature_unit, 40, node_config.font_color.r, node_config.font_color.g, node_config.font_color.b, node_config.font_color.a)
-        font:write(20, 610, "Sensor " .. humidity_identifier .. ": " .. node_sensors[1].values.humidity .. " %", 40, node_config.font_color.r, node_config.font_color.g, node_config.font_color.b, node_config.font_color.a)
-        font:write(20, 710, "Sensor " .. dew_point_identifier .. ": " .. node_sensors[1].values.dew_point .. " " .. temperature_unit, 40, node_config.font_color.r, node_config.font_color.g, node_config.font_color.b, node_config.font_color.a)
+        font:write(20, y_pos, "Sensor " .. name_identifier .. ": " .. node_sensors[1].sensor_title, font_size, node_config.font_color.r, node_config.font_color.g, node_config.font_color.b, node_config.font_color.a)
+        y_pos += font_size + y_gap
+        if node_config.show_sensor_types then
+            font:write(20, y_pos, "Sensor " .. type_identifier .. ": " .. node_sensors[1].sensor_type, font_size, node_config.font_color.r, node_config.font_color.g, node_config.font_color.b, node_config.font_color.a)
+            y_pos += font_size + y_gap
+        end
+        if node_config.show_sensor_times then
+            font:write(20, y_pos, "Sensor " .. time_identifier .. ": " .. node_sensors[1].sensor_time, font_size, node_config.font_color.r, node_config.font_color.g, node_config.font_color.b, node_config.font_color.a)
+            y_pos += font_size + y_gap
+        end
+        if isBitSet(node_sensors[1].sensor_display_units, 1) then
+            font:write(20, y_pos, "Sensor " .. temparature_identifier .. ": " .. node_sensors[1].values.temperature .. " " .. temperature_unit, font_size, node_config.font_color.r, node_config.font_color.g, node_config.font_color.b, node_config.font_color.a)
+            y_pos += font_size + y_gap
+        end
+        if isBitSet(node_sensors[1].sensor_display_units, 2) then
+            font:write(20, y_pos, "Sensor " .. humidity_identifier .. ": " .. node_sensors[1].values.humidity .. " %", font_size, node_config.font_color.r, node_config.font_color.g, node_config.font_color.b, node_config.font_color.a)
+            y_pos += font_size + y_gap
+        end
+        if isBitSet(node_sensors[1].sensor_display_units, 3) then
+            font:write(20, y_pos, "Sensor " .. dew_point_identifier .. ": " .. node_sensors[1].values.dew_point .. " " .. temperature_unit, font_size, node_config.font_color.r, node_config.font_color.g, node_config.font_color.b, node_config.font_color.a)
+            y_pos += font_size + y_gap
+        end
     end
 end
