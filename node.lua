f5659ed3cf5529a413872701aa44d743704eae2a
Jan Henrik Sawatzki <jhs@sawatzki-home.de>
2020-06-24 11:23:23 +0200

    Update node.lua

--- node.lua
+++ node.lua
@@ -121,10 +121,6 @@ end)
 
 util.json_watch("sensors.json", function(sensors)
     node_sensors = sensors
-    pp(node_sensors[1].sensor_display_units)
-    pp(isBitSet(node_sensors[1].sensor_display_units, 1))
-    pp(isBitSet(node_sensors[1].sensor_display_units, 2))
-    pp(isBitSet(node_sensors[1].sensor_display_units, 3))
 end)
 
 local function Clock()
