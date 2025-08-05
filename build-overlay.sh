8f10d91a1ad92a1a7ff44734b861839b1ae541bc
Jan Henrik Sawatzki <sawatzki@iabg.de>
2025-08-05 17:05:18 +0200

    Python3 precompile extended

--- build-overlay.sh
+++ build-overlay.sh
@@ -22,14 +22,40 @@ done
 for dir in \
     usr/lib/python3.11 \
     usr/lib/python3.11/asyncio \
+    usr/lib/python3.11/collections \
+    usr/lib/python3.11/ctypes \
+    usr/lib/python3.11/curses \
+    usr/lib/python3.11/dbm \
+    usr/lib/python3.11/distutils \
+    usr/lib/python3.11/email \
     usr/lib/python3.11/encodings \
+    usr/lib/python3.11/html \
+    usr/lib/python3.11/http \
     usr/lib/python3.11/importlib \
+    usr/lib/python3.11/logging \
     usr/lib/python3.11/multiprocessing \
+    usr/lib/python3.11/re \
+    usr/lib/python3.11/sqlite3 \
+    usr/lib/python3.11/tomllib \
     usr/lib/python3.11/urllib \
+    usr/lib/python3.11/venv \
+    usr/lib/python3.11/wsgiref \
+    usr/lib/python3.11/xml \
+    usr/lib/python3.11/xmlrpc \
+    usr/lib/python3.11/zoneinfo \
     usr/lib/python3/dist-packages \
     usr/lib/python3/dist-packages/certifi \
     usr/lib/python3/dist-packages/chardet \
+    usr/lib/python3/dist-packages/charset_normalizer \
+    usr/lib/python3/dist-packages/Cryptodome \
+    usr/lib/python3/dist-packages/dateutil \
     usr/lib/python3/dist-packages/idna \
+    usr/lib/python3/dist-packages/lxml \
+    usr/lib/python3/dist-packages/lxml/html \
+    usr/lib/python3/dist-packages/lxml/isoschematron \
+    usr/lib/python3/dist-packages/PIL \
+    usr/lib/python3/dist-packages/pkg_resources \
+    usr/lib/python3/dist-packages/pytz_deprecation_shim \
     usr/lib/python3/dist-packages/requests \
     usr/lib/python3/dist-packages/urllib3 \
     usr/lib/python3/dist-packages/urllib3/util \
